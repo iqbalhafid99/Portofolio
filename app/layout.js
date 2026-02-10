380d59b319a540386b11b0af0367d12350527cbd
iqbalhafid99 <mohiqbalhafid@gmail.com>
2026-02-10 17:38:25 +0700

    Update website

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="en" data-theme="light">
       <body>
         <Navbar />
-        <PageTransition>{children}</PageTransition>
+        {children}
       </body>
     </html>
   );
